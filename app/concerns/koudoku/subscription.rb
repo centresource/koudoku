183959fb730615384d82f34b7648799481b5987c
Brandon Valentine <brandon@brandonvalentine.com>
2016-05-03 00:25:45 -0500

    Revert "Merge remote-tracking branch 'civisanalytics/missing_token'"
    This reverts commit 9a62965b610b79c77d39802a0d5f0e4f5e68ce60, reversing
    changes made to 841614e01596fca6135dd5f26d1df9e22e6d0ea0.

--- app/concerns/koudoku/subscription.rb
+++ app/concerns/koudoku/subscription.rb
@@ -68,7 +68,6 @@ module Koudoku::Subscription
 
             begin
 
-              raise Koudoku::NilCardToken, "Possible javascript error" if credit_card_token.empty?
               customer_attributes = {
                 description: subscription_owner_description,
                 email: subscription_owner_email,
--- lib/koudoku/errors.rb
+++
@@ -1,4 +0,0 @@
-module Koudoku
-  class Error < StandardError; end
-  class NilCardToken < Error; end
-end
